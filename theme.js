8f3a25a711d25f09db9b5109760654ddc3d46c09
rahulratheesh91 <rahul.ratheesh91@gmail.com>
2026-02-06 16:03:37 +0000

    Delete theme.js

--- theme.js
+++
@@ -1,6 +0,0 @@
-const btn=document.getElementById('themeToggle');
-btn.onclick=()=>{
- document.body.classList.toggle('dark');
- localStorage.setItem('theme',document.body.className);
-};
-document.body.className=localStorage.getItem('theme')||'';
